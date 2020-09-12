d428a4fe40714cff52d2e9bf8e29593a3ecc9594
Ahmed Shalaby <kingshalaby@yahoo.com>
2020-09-12 09:57:24 +0200

    add user registration

---
+++ lib/rumbl/accounts.ex
@@ -0,0 +1,44 @@
+defmodule Rumbl.Accounts do
+  @moduledoc """
+  Accounts context module
+  """
+
+  alias Rumbl.Repo
+  alias Rumbl.Accounts.User
+
+  def list_users do
+    Repo.all(User)
+  end
+
+  def get_user(id) do
+    Repo.get(User, id)
+  end
+
+  def get_user!(id) do
+    Repo.get!(User, id)
+  end
+
+  def get_user_by(params) do
+    Repo.get(User, params)
+  end
+
+  def change_user(%User{} = user) do
+    User.changeset(user, %{})
+  end
+
+  def create_user(attrs \\ %{}) do
+    %User{}
+    |> User.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def change_registration(%User{} = user, params) do
+    User.registration_changeset(user, params)
+  end
+
+  def register_user(attrs \\ %{}) do
+    %User{}
+    |> User.registration_changeset(attrs)
+    |> Repo.insert()
+  end
+end
