cadbfa9fe64c30585d1afde2ac42c64ab78157fd
Ahmed Shalaby <kingshalaby@yahoo.com>
2020-11-21 02:35:15 +0200

    remove live dashboard

--- lib/rumbl_web/router.ex
+++ lib/rumbl_web/router.ex
@@ -46,7 +46,7 @@ defmodule RumblWeb.Router do
 
     scope "/" do
       pipe_through :browser
-      live_dashboard "/dashboard", metrics: RumblWeb.Telemetry
+ #     live_dashboard "/dashboard", metrics: RumblWeb.Telemetry
     end
   end
 end
